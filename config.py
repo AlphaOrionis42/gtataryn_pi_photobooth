6dee3323b55cec2bdc8c784322541c7fe98ab4c1
AlphaOrionis42 <gregtataryn@gmail.com>
2017-09-25 07:27:24 -0400

    Began adding functionality to play images as screen saver

--- config.py
+++ config.py
@@ -1,19 +1,32 @@
 # Config file for PhotoBooth app
-save_path = '/media/pi/P/pics/' # Path to save the photos
-gif_path = '/media/pi/P/gifs/' # Path for gifs
-slide_path = '/home/pi/gtataryn_pi_photobooth/slides/' # Path for instructional and counddownslides
+# Author: Greg Tataryn
+
+# Resolution Settings
 monitor_h = 480
 monitor_w = 800
+photo_w = 1640
+photo_h = 1232
+
 btnPin = 11
 greenLed = 13
 redLed = 15
 debounce = 0.25
 
 # Camera config
-camera_iso = 800
-num_shots = 3
+camera_iso = 800 # ISO setting for camera
+num_shots = 3 # How many shots to take
 
 # Other constants
-gif_delay = 100
-replay_count = 2
-replay_wait = 1
+gif_delay = 100 # Delay between shots for GIF in miliseconds
+
+# Replay variables
+replay_count = 2 # Number of loops through the photo set
+replay_wait = 1 # Delay to show each photo in seconds
+
+# File path variables
+save_path = '/media/pi/P/pics/' # Path to save the photos
+gif_path = '/media/pi/P/gifs/' # Path for gifs
+slide_path = '/home/pi/gtataryn_pi_photobooth/slides/' # Path for instructional and counddown slides
+
+prep_delay = 2 # Delay for people to pose in seconds
+shot_delay = 1 # Delay between photo captures in seconds
--- photobooth.py
+++ photobooth.py
@@ -3,6 +3,7 @@
 
 import os
 import config
+import fnmatch
 import RPi.GPIO as GPIO
 import picamera
 import sys
@@ -18,17 +19,9 @@ debug = True
 # Run variable for main program
 run = True
 
-###################
-# Other Variables #
-###################
-transform_x = config.monitor_w
-transform_y = config.monitor_h
-offset_x = 0
-offset_y = 0
-
-##############
-# GPIO Setup #
-##############
+#######################
+# GPIO Initialization #
+#######################
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(config.btnPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(config.greenLed, GPIO.OUT)
@@ -36,26 +29,39 @@ GPIO.setup(config.redLed, GPIO.OUT)
 GPIO.output(config.greenLed, False)
 GPIO.output(config.redLed, False)
 
-################
-# Pygame Setup #
-################
+#########################
+# Pygame Initialization #
+#########################
 pygame.init()
 pygame.display.set_mode((config.monitor_w, config.monitor_h))
 screen = pygame.display.get_surface()
 pygame.mouse.set_visible(False)
 pygame.display.toggle_fullscreen()
 
+################################################
+# Function to clear the slides from the screen #
+################################################
 def clear_screen():
     screen.fill( (0,0,0) )
     pygame.display.flip()
-    
+
+##################################################
+# Function to check for input to end the program #
+# Use the escape key to exit                     #
+##################################################
 def chk_input(events):
     for event in events:
         if (event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE)):
             run = False
             pygame.quit()
             sys.exit()
+        elif event.type == MOUSEBUTTONDOWN:
+            print("Mouse button pressed")
+            return True
 
+####################################################
+# Function to overlay images to the camera preview #
+####################################################
 def overlay_img(imgPath, cam):
     img = Image.open(imgPath)
     oLay = cam.add_overlay(img.tobytes(), size=img.size)
@@ -96,17 +102,33 @@ def show_slide(img_path, offset_x, offset_y):
     screen.blit(img, (offset_x, offset_y))
     pygame.display.flip()
 
+###############################################
+# Function to make animated GIF of the photos #
+###############################################
 def make_gif(jpg_group, ts):
     if debug:
         print('Making animated gif')
-    for x in range(1, config.num_shots+1):
-        graphicsmagick = "gm convert -size 800x480 " + jpg_group + "_" + str(x) + ".jpg -thumbnail 800x480 " + jpg_group + "_" + str(x) + "_sm.jpg"
-        os.system(graphicsmagick)
     
-    graphicsmagick = "gm convert -delay " + str(config.gif_delay) + " " + jpg_group + "*.jpg" + " " + config.gif_path + "photobooth_" + ts + ".gif"
-    #print(graphicsmagick)
+    graphicsmagick = "gm convert -delay " + str(config.gif_delay) + " " + \
+        jpg_group + "*.jpg" + " " + config.gif_path + "photobooth_" + ts + \
+        ".gif"
     os.system(graphicsmagick)
 
+#############################
+# Function to resize photos #
+#############################
+def resize_imgs(jpg_group):
+    for x in range(1, config.num_shots+1):
+        # Use Graphics Magick to resize the files for display on a screen. Uses the monitor resolution from confic.py
+        graphicsmagick = "gm convert -size " + config.monitor_w + "x" + \
+            config.monitor_h + " " + jpg_group + "_" + str(x) + \
+            ".jpg -thumbnail " + config.monitor_w + "x" + config.monitor_h + \
+            " " + jpg_group + "_" + str(x) + "_sm.jpg"
+        os.system(graphicsmagick)
+
+###################################################
+# Function to display the preview images to users #
+###################################################
 def display_imgs(jpg_group):
     for i in range(0, config.replay_count):
         for j in range(1,config.num_shots+1):
@@ -114,6 +136,43 @@ def display_imgs(jpg_group):
             show_slide(jpg_group + "_" + str(j) + "_sm.jpg", 80, 0)
             sleep(config.replay_wait)
 
+##############################################
+# Function to run slide show of photos taken #
+##############################################
+def slide_show():
+    file_list = os.listdir(config.save_path)
+    num_files = len(file_list)
+    slide_count = 0
+    while True:
+        if chk_input(pygame.event.get()):
+            print("Mous pressed, going back to main")
+            break
+        if num_files >= 3:
+            for file in file_list:
+                if chk_input(pygame.event.get()):
+                    print("Mouse pressed, going back to while true")
+                    break
+                if fnmatch.fnmatch(file, '*sm.jpg'):
+                    show_slide(config.save_path + file, 0, 0)
+                    sleep(config.replay_wait)
+                    slide_count += 1
+
+                if num_files < 6 and slide_count == num_files:
+                    show_slide(config.slide_path + 'start.png', 0, 0)
+                    sleep(config.prep_delay)
+                    slide_count = 0
+                elif slide_count == 6:
+                    show_slide(config.slide_path + 'start.png', 0, 0)
+                    sleep(config.prep_delay)
+                    slide_count = 0
+        else:
+            break
+    return
+                    
+
+#############################
+# Function to run the booth #
+#############################
 def run_booth():
     if debug:
         print("Running photobooth")
@@ -132,14 +191,13 @@ def run_booth():
         for i in range(1,config.num_shots+1):
             GPIO.output(config.redLed, False)
             camera.hflip = True
-            #camera.sensor_mode = 4
-            camera.resolution = (800, 480)
+            camera.resolution = (config.monitor_w, config.monitor_h)
             camera.start_preview()
             filename = config.save_path + 'photobooth_' + now + '_' + str(i) + '.jpg'
             sleep(.5)
             o = overlay_img(config.slide_path + 'pose.png', camera)
             GPIO.output(config.greenLed, True)
-            sleep(2)
+            sleep(config.prep_delay)
             camera.remove_overlay(o)
             GPIO.output(config.greenLed, False)
             sleep(.5)
@@ -163,12 +221,12 @@ def run_booth():
             sleep(.5)
             camera.hflip = False
             camera.stop_preview()
-            camera.resolution = (1640, 1232)
+            camera.resolution = (config.photo_w, config.photo_h)
             GPIO.output(config.redLed, True)
             camera.capture(filename)
             if debug:
                 print(filename)
-            time.sleep(1)
+            time.sleep(config.shot_delay)
     except Exception as err:
         print("Error in function run_booth")
         print(type(err))
@@ -181,11 +239,11 @@ def run_booth():
         camera.close()
 
     show_slide(config.slide_path + "wait.png", 0, 0)
+    resize_imgs(config.save_path + "photobooth_" + now)
     make_gif(config.save_path + "photobooth_" + now, now)
     clear_screen()
     display_imgs(config.save_path + "photobooth_" + now)
-
-    
+    clear_screen()    
     show_slide(config.slide_path + 'intro.png', 0, 0)
 
 # Function that runs on startup to blink the LED.
@@ -205,11 +263,13 @@ try:
     Startup()
     time.sleep(.25)
     show_slide(config.slide_path + 'intro.png', 0, 0)
+    sleep(config.prep_delay)
     while run:
         GPIO.output(config.redLed, False)
         GPIO.output(config.greenLed, True)
         chk_input(pygame.event.get())
         #print("Green LED on")
+        slide_show()
         if GPIO.input(config.btnPin):
             time.sleep(config.debounce)
             if debug:
