b6e9ad9b16896c6ee5e91c74f4fed4bbc5e1630c
AlphaOrionis42 <gregtataryn@gmail.com>
2017-09-25 20:40:58 -0400

    Updated comments, added back overlay_img function that was removed incorrectly. Updated function names to be more meaningful.

--- photobooth.py
+++ photobooth.py
@@ -13,12 +13,11 @@ import time
 from time import sleep
 from PIL import Image
 
-# Debug variable used in testing. Set to false for production
-debug = True
-
-# Run variable for main program
-run = True
-run_show = True
+# Local variables
+debug = False #Debug toggle
+run = True #Run variable for main program
+run_show = True # Run variable for the slide show
+last_img = 0 # Used inside the slide show to track the last photo shown
 
 #######################
 # GPIO Initialization #
@@ -43,6 +42,7 @@ pygame.display.toggle_fullscreen()
 # Function to clear the slides from the screen #
 ################################################
 def clear_screen():
+    # Fill the screen with black
     screen.fill( (0,0,0) )
     pygame.display.flip()
 
@@ -52,25 +52,35 @@ def clear_screen():
 ##################################################
 def chk_input(events):
     for event in events:
+        # If the input received was an escape key, then exit the program.
         if (event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE)):
             run = False
             pygame.quit()
             sys.exit()
+        # If the input was a mouse button press, return true
+        # used for ending the slide show when users tap the touch screen.
         elif event.type == MOUSEBUTTONDOWN:
             print("Mouse button")
             return True
+
+####################################################
+# Function to overlay images to the camera preview #
+####################################################
+def overlay_img(imgPath, cam):
+    img = Image.open(imgPath)
+    oLay = cam.add_overlay(img.tobytes(), size=img.size)
+    oLay.alpha = 100
+    oLay.layer = 3
+    return oLay
+
 #############################################
 # Function to display an image using pygame #
 #############################################
 def show_img(img_path, offset_x, offset_y):
-    screen.fill( (0,0,0) )
+    clear_screen()
 
     img = pygame.image.load(img_path)
     img = img.convert()
-
-    #set_img_dimension(int(img.get_width()), int(img.get_height()))
-
-    #img = pygame.transform.scale(img, (transform_x, transform_y))
 
     screen.blit(img, (offset_x, offset_y))
     pygame.display.flip()
@@ -91,12 +101,14 @@ def make_gif(jpg_group, ts):
 # Function to resize photos #
 #############################
 def resize_imgs(jpg_group):
+    print("Resizing images")
     for x in range(1, config.num_shots+1):
         # Use Graphics Magick to resize the files for display on a screen. Uses the monitor resolution from confic.py
-        graphicsmagick = "gm convert -size " + config.monitor_w + "x" + \
-            config.monitor_h + " " + jpg_group + "_" + str(x) + \
-            ".jpg -thumbnail " + config.monitor_w + "x" + config.monitor_h + \
+        graphicsmagick = "gm convert -size " + str(config.monitor_w) + "x" + \
+            str(config.monitor_h) + " " + jpg_group + "_" + str(x) + \
+            ".jpg -thumbnail " + str(config.monitor_w) + "x" + str(config.monitor_h) + \
             " " + jpg_group + "_" + str(x) + "_sm.jpg"
+        print(graphicsmagick)
         os.system(graphicsmagick)
 
 ###################################################
@@ -109,27 +121,41 @@ def img_replay(jpg_group):
             show_img(jpg_group + "_" + str(j) + "_sm.jpg", 80, 0)
             sleep(config.replay_wait)
 
-##############################################
-# Function to run slide show of photos taken #
-##############################################
+###################################################
+# Function to run slide show of  all photos taken #
+###################################################
 def run_slide_show():
     file_list = os.listdir(config.save_path)
     num_files = len(file_list)
     slide_count = 0
+    global last_img
+    if debug:
+        print(str(last_img))
     run_show = True
     while run_show:
         if chk_input(pygame.event.get()):
             run_show = False
             break
         if num_files >= 3:
-            for file in file_list:
+            if last_img > num_files:
+                if debug:
+                    print("Last image higher than number of files")
+                    print(str(last_img))
+                last_img = 0
+                if debug:
+                    print(str(last_img))
+            for i in range(last_img,num_files):
+                if debug:
+                    print("i = " + str(i))
+                    print("last_img = " + str(last_img))
                 if chk_input(pygame.event.get()):
                     run_show = False
                     break
-                if fnmatch.fnmatch(file, '*sm.jpg'):
-                    show_img(config.save_path + file, 0, 0)
+                if fnmatch.fnmatch(file_list[i], '*sm.jpg'):
+                    show_img(config.save_path + file_list[i], 0, 0)
                     sleep(config.replay_wait)
                     slide_count += 1
+                    last_img = i
 
                 if num_files < 6 and slide_count == num_files:
                     show_img(config.slide_path + 'start.png', 0, 0)
@@ -140,7 +166,7 @@ def run_slide_show():
                     sleep(config.prep_delay)
                     slide_count = 0
         else:
-            run_show= False
+            run_show = False
             break
     show_img(config.slide_path + 'intro.png', 0, 0)
                     
@@ -201,7 +227,7 @@ def run_booth():
             camera.capture(filename)
             if debug:
                 print(filename)
-            time.sleep(config.shot_delay)
+            sleep(config.shot_delay)
     except Exception as err:
         print("Error in function run_booth")
         print(type(err))
@@ -212,16 +238,20 @@ def run_booth():
     finally:
         camera.stop_preview()
         camera.close()
-
+    print("Wait for resize")
     show_img(config.slide_path + "wait.png", 0, 0)
     resize_imgs(config.save_path + "photobooth_" + now)
+    print("Wait for GIF")
     make_gif(config.save_path + "photobooth_" + now, now)
     clear_screen()
     img_replay(config.save_path + "photobooth_" + now)
     clear_screen()    
     show_img(config.slide_path + 'intro.png', 0, 0)
 
-# Function that runs on startup to blink the LED.
+##################################################
+# Function that runs on startup to blink the LED #
+# Lets us know everything is working.            #
+##################################################
 def Startup():
     for x in range(1,5):
         GPIO.output(config.greenLed, True)
@@ -230,13 +260,24 @@ def Startup():
         time.sleep(0.25)
     GPIO.output(config.redLed, True)
 
+###################################################
+# Function to handle waiting for the button press #
+# Taks in a wait_time in seconds for how long it  #
+# should wait for a button press.                 #
+###################################################
 def wait_for_button(wait_time):
     display_timer = time.time() + wait_time
+    btn_pressed = False
     while time.time() <= display_timer:
         chk_input(pygame.event.get())
         if GPIO.input(config.btnPin):
             sleep(config.debounce)
+            btn_pressed = True
             run_booth()
+        chk_input(pygame.event.get())
+    # If the button was pressed, wait for another press before proceeding on
+    if btn_pressed:
+        wait_for_button(5)
 
 ################
 # Main Program #
@@ -244,14 +285,16 @@ def wait_for_button(wait_time):
 try:
     # Run startup to blink the green light.
     Startup()
-    time.sleep(.25)
+    time.sleep(0.5)
     show_img(config.slide_path + 'intro.png', 0, 0)
-    sleep(config.prep_delay)
     while run:
+        # Turn off the red LED and turn on the green LED.
         GPIO.output(config.redLed, False)
         GPIO.output(config.greenLed, True)
+        # Check for input to exit, then wait for the button press.
         chk_input(pygame.event.get())
         wait_for_button(5)
+        # Check again for input to exit and begin the slide show.
         chk_input(pygame.event.get())
         run_slide_show()
 except KeyboardInterrupt:
